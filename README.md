146b46d8199a82bf3d2220abfb038b1834af3be3
leandrovitor2022 <vitordasilvaleandro38@gmail.com>
2022-07-14 19:02:51 -0300

    first commit

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # calcula_salario.py
 # calcula_salario.py
 # calcula_salario.py
+# calcula_salario.py
---
+++ calcula_salario.py
@@ -0,0 +1,17 @@
+hora = float (input("Quanto vc ganha por hora? "))
+
+horas_trabalhadas = int (input("Quantas horas voce trabalha por semana? "))
+
+salario_bruto = hora * horas_trabalhadas
+inss= (8*salario_bruto)/100
+sindicato = (5*salario_bruto)/100
+ir = (11*salario_bruto)/100
+descontos = inss + sindicato + ir 
+salario_liquido = salario_bruto - descontos
+#pega a hora e as horas trabalhadas e calcula o salario líquido
+
+
+# aqui vc mostra o valor bruto calculado
+print("Voce ganha por mês R$:" + str(salario_bruto))
+print("Voce pagou  de INSS R$: " + str(inss))
+print("Voce pagou ao sindicato R$: "  + str(sindicato))
